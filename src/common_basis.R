ed02f97d40a320ddb75047dfb0d7015b702242c1
Eetu Mäkelä <eetu.makela@iki.fi>
2024-02-13 15:32:45 +0200

    initial commit

---
+++ src/common_basis.R
@@ -0,0 +1,26 @@
+library(here)
+library(tidyverse)
+library(googlesheets4)
+library(gghsci)
+library(hscidbutil)
+library(plotly)
+library(glue)
+library(svglite)
+library(gt)
+library(googledrive)
+library(plotly)
+
+save_plots <- function(dir,basename,plot,width=6,height=5,units="in",dpi=300) {
+  ggsave(glue("{dir}/{basename}.png"),plot,width=width,height=height,dpi=dpi, units=units)
+  ggsave(glue("{dir}/{basename}.svg"),plot,width=width,height=height,dpi=dpi, units=units)
+  ggsave(glue("{dir}/{basename}.pdf"),plot,width=width,height=height,dpi=dpi, units=units, device = cairo_pdf)
+  knitr::include_graphics(glue("{dir}/{basename}.pdf"), dpi=dpi)
+}
+
+if (!exists("con")) con <- get_connection()
+register_tables(con, "fbs_analysis")
+
+local({
+  temp_tables <- list_temporary_tables(con, "fbs_analysis")
+  if (nrow(temp_tables) > 0) warning("The following temporary tables were found in the database. Use delete_temporary_tables() to remove.\n", temp_tables)
+})
